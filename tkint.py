cb9a9bbdce391646b1c40155299956d8a7216980
Wyatt Homola <whomola@cvsu.org>
2022-03-14 11:02:45 -0400

    gitignore

---
+++ tkint.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import matplotlib
+# Necessary to get pyplot and tkinter to play well.
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+root = tk.Tk()
+
+root.mainloop()
